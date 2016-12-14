537bc9d524c4e779053649d090dda049a041404b
James Ahlm <14782828428@qq.com>
2016-12-14 10:26:01 +0800

    Shows Camper Data

--- app/controllers/dist/index.js
+++ app/controllers/dist/index.js
@@ -1,7 +1,63 @@
+var recent = "https://fcctop100.herokuapp.com/api/fccusers/top/recent"
+var allTime = "https://fcctop100.herokuapp.com/api/fccusers/top/alltime"
 
 
 
-      ReactDOM.render(
-        <DisplayContainer />,
-        document.getElementById('container')
-      );
+var Scoreboard = React.createClass({
+  loadUsers(){
+    this.serverRequest = $.get(recent, function(result){
+      this.setState({
+        campers:result
+      })
+    }.bind(this))
+  },
+  getInitialState(){
+    return ({campers:[]})
+  },
+  componentDidMount: function() {
+    this.loadUsers();
+  },
+  render(){
+    console.log(this.state.block)
+    
+    var campers = this.state.campers.map(function(c, i){
+	return <Camper key={i} rank={i} name={c.username} recentScore={c.recent} totalScore={c.alltime} />
+})
+
+    return (
+      <table className="table table-hover">
+        <thead>
+	  <tr>
+	    <th>#</th>
+	    <th>Name</th>
+	    <th>Recent Score</th>
+	    <th>Total Score</th>
+	  </tr>
+	</thead>
+        <tbody>
+        {campers}
+	</tbody>
+      </table>
+    )
+    
+  }
+});
+
+var Camper = React.createClass({
+  
+  render(){
+    return (
+      <tr>
+        <th>{this.props.rank}</th>
+        <td>{this.props.name}</td>
+        <td>{this.props.recentScore}</td>
+        <td>{this.props.totalScore}</td>
+      </tr>
+    )
+  }
+});
+
+ReactDOM.render(
+  <Scoreboard url="https://fcctop100.herokuapp.com/api/fccusers/top/recent"  pollInterval={2000}/>,
+  document.getElementById('container')
+);
